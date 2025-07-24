c39f4eb06c9ef4adda7a6bde1a4429a9ee722707
King-MRG1 <MohamedRamdan2214@outlook.com>
2025-07-24 17:36:30 +0300

    Initial commit

--- obj/Debug/net9.0/To-Do_List.AssemblyInfo.cs
+++ obj/Debug/net9.0/To-Do_List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-Do_List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce4674b347b9abed7631d0c196066f681b1f7d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-Do_List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To-Do_List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
